249013f797bfba6bbec7af0d2cd221c44c117536
SatyamAK <satyam.ak99@gmail.com>
2021-06-01 19:35:09 +0530

    Firebase Setup done

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:panik_musik/bottomnavbar.dart';
 import 'package:panik_musik/constants/theme.dart';
 import 'package:panik_musik/screens/login/loginscreen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 

6d11762124d4366eec60a157d9a63149a8de95df
SatyamAK <satyam.ak99@gmail.com>
2022-01-10 12:32:48 +0530

    Home Page UI done

---
+++ lib/components/cards.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+SizedBox collectionCard(String title, img, BuildContext context) {
+  return SizedBox(
+    height: 128,
+    width: 128,
+    child: Card(
+      color: Theme.of(context).backgroundColor.withOpacity(0.2),
+      margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            height: 128,
+            width: 124,
+            margin: const EdgeInsets.only(bottom: 8, left: 8, right: 8),
+            child: Image.network(img, fit: BoxFit.cover,)
+          ),
+          Text(title, style: Theme.of(context).textTheme.button)
+        ],
+      ),
+    ),
+  );
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,12 +1,78 @@
 import 'package:flutter/material.dart';
+import 'package:panik_musik/components/cards.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({ Key? key }) : super(key: key);
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Home', style: Theme.of(context).textTheme.bodyText1,)
-    );
+    return SafeArea(
+        child: SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.only(left: 8, right: 8, top: 18),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text('Animes', style: Theme.of(context).textTheme.headline1),
+            Flexible(
+              child: Container(
+                margin: const EdgeInsets.only(top: 12, bottom: 12),
+                height: 200,
+                child: ListView.builder(
+                    itemCount: 3,
+                    shrinkWrap: true,
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, index) {
+                      return collectionCard(
+                          'title',
+                          'https://i.insider.com/5e820b04671de06758588fb8?width=700',
+                          context
+                      );
+                  }
+                ),
+              ),
+            ),
+            Text('Video Games', style: Theme.of(context).textTheme.headline1),
+            Flexible(
+              child: Container(
+                margin: const EdgeInsets.only(top: 12, bottom: 12),
+                height: 200,
+                child: ListView.builder(
+                    itemCount: 3,
+                    shrinkWrap: true,
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, index) {
+                      return collectionCard(
+                          'title',
+                          'https://i.insider.com/5e820b04671de06758588fb8?width=700',
+                          context
+                      );
+                  }
+                ),
+              ),
+            ),
+            Text('Artists', style: Theme.of(context).textTheme.headline1),
+            Flexible(
+              child: Container(
+                margin: const EdgeInsets.only(top: 12, bottom: 12),
+                height: 200,
+                child: ListView.builder(
+                    itemCount: 3,
+                    shrinkWrap: true,
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, index) {
+                      return collectionCard(
+                          'title',
+                          'https://i.insider.com/5e820b04671de06758588fb8?width=700',
+                          context
+                      );
+                  }
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    ));
   }
 }
